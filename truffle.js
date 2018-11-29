c82f09aaeee83b6ddb5f95110237af8309c563a1
yenming <yenming0212@gmail.com>
2018-11-29 20:38:49 +0800

    修改網路設定 - truffle.js

--- truffle.js
+++ truffle.js
@@ -6,6 +6,13 @@ module.exports = {
       host: "127.0.0.1",
       port: 7545,
       network_id: "*" // Match any network id
+    },
+    travis: {
+      host: 'localhost',
+      port: 8545,
+      network_id: '*',
+      from :'0x7eff122b94897ea5b0e2a9abf47b86337fafebdc',
+      gas: '4000000'
     }
   }
 };
